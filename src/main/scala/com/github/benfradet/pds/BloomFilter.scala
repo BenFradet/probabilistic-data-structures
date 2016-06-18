f3a2bcf3c8955506c6eacd98d375b090a826ce0e
BenFradet <benjamin.fradet@gmail.com>
2016-06-18 14:49:26 +0200

    rmd wikipedia extracts

--- src/main/scala/com/github/benfradet/pds/BloomFilter.scala
+++ src/main/scala/com/github/benfradet/pds/BloomFilter.scala
@@ -4,37 +4,7 @@ import scala.collection.mutable
 import scala.util.hashing.MurmurHash3
 
 /**
- * From: https://en.wikipedia.org/wiki/Bloom_filter
- *
- * A Bloom filter tests if an element is a member of a set.
- *
- * False positives are possible (returns that an element is in the set while it's not) but false
- * negatives are not possible (does not return that an element is in the set if it's not).
- * Elements can be added to the set but not removed.
- * The more elements are added to the set, the larger the probability of false positives.
- *
- * An empty Bloom filter is a bit array of ''m'', all set to 0 and ''k'' different hash functions
- * each of which maps some set element to one of the ''m'' array positions with a uniform random
- * distribution.
- * Typically, ''k'' is a much smaller than ''m'', which is proportional to the number of elements to
- * be added, the precise choice of ''k'' and the constant of proportionality of ''m'' are
- * determined by the intended false positive rate of the filter.
- *
- * To ''add'' an element, feed it to each of the ''k'' hash functions to get ''k'' array positions
- * which are consequently set to 1 (O(k) complexity).
- *
- * To ''query'' for an element, feed it to each of the ''k'' hash functions to get ''k'' array
- * positions (O(k) complexity). If any of the bits is 0, the element is definitely not in the set.
- * If all bits are 1, then either the element is in the set, or the bits have by chance been set to
- * 1 during the insertion of other elements, resulting in a false positive.
- *
- * The probability of false positives is given by pow((1 - exp(-kn/m)), k)
- *
- * Given ''m'' and ''n'', the optimal ''k'' is m/n * ln 2
- *
- * Given the wanted percentage of false positives ''p'' and ''n'', ''m'' is
- * - (n ln p) / pow(ln 2, 2)
- *
+ * Intro: https://en.wikipedia.org/wiki/Bloom_filter
  * This implementation has been heavily inspired by Breeze's:
  * https://github.com/scalanlp/breeze/blob/master/math/src/main/scala/breeze/util/BloomFilter.scala
  */
--- src/main/scala/com/github/benfradet/pds/CountMinSketch.scala
+++ src/main/scala/com/github/benfradet/pds/CountMinSketch.scala
@@ -4,40 +4,9 @@ import scala.math._
 import scala.util.hashing.MurmurHash3
 
 /**
- * From: https://en.wikipedia.org/wiki/Count%E2%80%93min_sketch
- *
- * CountMinSketch counts the frequency of events in a stream of data.
- *
- * It uses hash functions to map events to frequencies, using only sub-linear space, at the cost
- * of overcounting some events due to collisions.
- *
- * The goal is to consume a stream of events, one at a time, and count the frequency of the
- * different types of events in the stream.
- * At any time, the sketch can be queried for frequency of a particular event type ''i'', and
- * will return an estimate of this frequency that is within a certain distance of the true
- * frequency, with a certain probability.
- *
- * The underlying data structure is a two-dimensional array of ''w'' columns and ''d'' rows. The
- * parameters ''w'' and ''d'' are fixed when the sketch is created, and determine the time and
- * space needs and the probability of error when the sketch is queried. Associated with each of
- * the ''d'' rows is a separate hash function. The hash functions must be pairwise independent.
- * The parameters ''w'' and ''d'' can be chosen by setting ''w = ceil(e / eps)'' and
- * ''d = ceil(1 / delta)'', where the error in answering a query is within a factor of ''eps''
- * with probability ''delta''.
- *
- * When a new event of type ''i'' arrives we update as follows: for each of row ''j'' of the
- * table, apply the corresponding hash function to obtain a column index ''k = h_j(i)''. Then
- * increment the value in row ''j'', column ''k'' by one.
- *
- * The point query asks for the count of event type ''i''. The estimated count is given by the
- * least value in the table for i, namely ''â_i = min_j count[j, h_j(i)]'' where ''count'' is
- * the table. This estimate has the guarantee that ''â_i <= a_i + eps sum_j^n(a_j)'' with
- * probability ''1 - delta'' where ''a_i'' is the true frequency with which ''i'' occurred in the
- * stream.
- *
+ * Intro: https://en.wikipedia.org/wiki/Count%E2%80%93min_sketch
  * This implementation has been by inspired Algebird's:
  * https://github.com/twitter/algebird/blob/develop/algebird-core/src/main/scala/com/twitter/algebird/CountMinSketch.scala
- *
  * Reference paper: http://dimacs.rutgers.edu/~graham/pubs/papers/cmencyc.pdf
  */
 class CountMinSketch[T](w: Int, d: Int) {
