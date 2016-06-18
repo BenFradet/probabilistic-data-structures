3e1bba149ca8704eda77857681cc0ff4cf3886e4
BenFradet <benjamin.fradet@gmail.com>
2016-06-18 11:43:37 +0200

    first implem of hll

---
+++ src/main/scala/com/github/benfradet/pds/HyperLogLog.scala
@@ -0,0 +1,75 @@
+package com.github.benfradet.pds
+
+import scala.math._
+import scala.util.hashing.MurmurHash3
+
+/** @param numBits number of bits to use */
+class HyperLogLog[T](val numBits: Int) {
+  /** @param rsd relative standard deviation */
+  def this(rsd: Double) = this(nbBits(rsd))
+
+  val rsd = 1.106 / sqrt(exp(numBits * log(2)))
+  val accuracy = 1.04 / sqrt(pow(2, numBits))
+
+  private val bitSet = new BitSet(1 << numBits)
+
+  def add(elem: T): Unit = {
+    val hash = MurmurHash3.mixLast(0, elem.##)
+    // j is the binary address determined by the first b bits of hash between 0 and and 2^b
+    val j = hash >>> (Integer.SIZE - numBits)
+    val r = Integer.numberOfLeadingZeros((hash - numBits) | (1 << (numBits - 1)) + 1) + 1
+    bitSet.updateIfGreater(j, r)
+  }
+
+  def cardinality: Long = {
+    val count = 1 << numBits
+    val values = (0 until count).map(bitSet.get)
+    val sum = values.foldLeft(0d)((s, v) => s + 1.0 / (1 << v))
+    val zeros = values.count(_ == 0).toDouble
+    val estimate = alphaMM(numBits, 1 << numBits) * (1 / sum)
+    import scala.math._
+    if (estimate <= (5.0 / 2.0) * count) round(count * log(count / zeros))
+    else round(estimate)
+  }
+
+  private val nbBits = (rsd: Double) => (log((1.106 / rsd) * (1.106 / rsd)) / log(2)).toInt
+  private val alphaMM = (p: Int, m: Int) => m * m * p match {
+    case 4 => 0.673
+    case 5 => 0.697
+    case 6 => 0.709
+    case _ => 0.7213 / (1 + 1.079 / m)
+  }
+}
+
+class BitSet(count: Int) {
+  private val array = new Array[Int](sizeForCount(count))
+  private val LOG2_BITS_PER_WORD = 6
+  private val REGISTER_SIZE = 5
+
+  def updateIfGreater(pos: Int, value: Int): Unit = {
+    val bucket = getBucket(pos)
+    val shift = getShift(pos, bucket)
+    val mask = 0x1f << shift
+
+    val curVal: Long = array(bucket) & mask
+    val newVal: Long = value << shift
+    if (curVal < newVal) array(bucket) = ((array(bucket) & ~mask) | newVal).toInt
+  }
+
+  def get(pos: Int): Int = {
+    val bucket = getBucket(pos)
+    val shift = getShift(pos, bucket)
+    (array(bucket) & (0x1f << shift)) >>> shift
+  }
+
+  private def sizeForCount(count: Int): Int = {
+    val bits = count / LOG2_BITS_PER_WORD
+    if (bits == 0) 1
+    else if (bits % Integer.SIZE == 0) bits
+    else bits + 1
+  }
+
+  private def getBucket(pos: Int): Int = pos / LOG2_BITS_PER_WORD
+  private def getShift(pos: Int, bucket: Int): Int =
+    REGISTER_SIZE * (pos - (bucket * LOG2_BITS_PER_WORD))
+}
